3986e5e2d1cedd8033d25b4710fbac610ebc9269
Howard Deiner <howard.deiner@deinersoft.com>
2017-05-18 17:12:03 -0400

    Initial commit.

--- src/test/java/test/com/solutionsiq/computers/monadic/springboot/InversionOfControlUsingSpringBootTest.java
+++ src/test/java/test/com/solutionsiq/computers/monadic/springboot/InversionOfControlUsingSpringBootTest.java
@@ -1,8 +1,6 @@
 package test.com.solutionsiq.computers.monadic.springboot;
 
 import com.solutionsiq.computers.monadic.springboot.consumer.ComputingApplication;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,22 +11,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @ComponentScan("com.solutionsiq.computers.monadic.springboot")
 public class InversionOfControlUsingSpringBootTest {
 
-    private AnnotationConfigApplicationContext context = null;
-
-    @Before
-    public void setUp() throws Exception {
-        context = new AnnotationConfigApplicationContext(InversionOfControlUsingSpringBootTest.class);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        context.close();
-    }
-
     @Test
-    public void test() {
+    public void checkDoublerService() {
+        AnnotationConfigApplicationContext context;
+        context = new AnnotationConfigApplicationContext(InversionOfControlUsingSpringBootTest.class);
         ComputingApplication app = context.getBean(ComputingApplication.class);
         assertThat(app.compute(8),is((long)16));
+        context.close();
     }
 
 }
