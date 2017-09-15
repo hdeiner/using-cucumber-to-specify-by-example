587c6e4cc415e6e61f20e36c9e2a9607cafced6b
Howard Deiner <howard.deiner@deinersoft.com>
2017-09-15 10:20:04 -0400

    Better approximate time acceptance criteria and better code to implement that.

--- src/main/java/com/solutionsiq/timeofday/TimeOfDayService.java
+++ src/main/java/com/solutionsiq/timeofday/TimeOfDayService.java
@@ -4,26 +4,30 @@ import com.solutionsiq.timeofday.definition.TimeOfDay;
 
 public class TimeOfDayService implements TimeOfDay {
 
-    public String getFormattedTimeOfDaySpokenWords(int hour, int minute, int second) {
-        String[] hours = {"twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"};
-        String[] approximateTimeWords = {"", "almost ten after", "ten after", "a quarter after", "twenty after", "almost half past", "half past", "almost twenty before", "twenty before", "a quarter of", "ten of", "almost"};
-        String[] dayQuadrant = {"at night", "in the morning", "in the afternoon", "in the evening"};
+    private static final int SECONDS_IN_A_HALF_MINUTE = 30;
+    private static final int HOURS_IN_A_QUARTER_OF_A_DAY = 6;
+    private static final int MINUTES_TO_ROUND_TO = 5;
+    private static final int MINUTE_TO_START_FUZZING_INTO_NEXT_HOUR = 35;
 
-        if (second >= 30) minute++;
+    private static final String[] NAMES_OF_THE_HOURS = {"twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"};
+    private static final String[] FUZZY_TIME_WORDS = {"about", "a little after", "about ten after", "about a quarter after", "about twenty after", "almost half past", "about half past", "almost twenty before", "about twenty before", "about a quarter of", "about ten of", "almost", "about"};
+    private static final String[] QUADRANTS_OF_THE_DAY = {"at night", "in the morning", "in the afternoon", "in the evening"};
 
-        String output = "";
-        if (minute >= 3) {
-            output += approximateTimeWords[(minute+2)/5] + " ";
-        }
-        if (minute < 35) {
-            output += hours[hour%12];
+    public String getFormattedTimeOfDaySpokenWords(int hour, int minute, int second){
+        String formattedTime = "";
+
+        if (second >= SECONDS_IN_A_HALF_MINUTE) minute++;
+
+        formattedTime += FUZZY_TIME_WORDS[(minute+(MINUTES_TO_ROUND_TO/2))/MINUTES_TO_ROUND_TO] + " ";
+
+        if (minute < MINUTE_TO_START_FUZZING_INTO_NEXT_HOUR) {
+            formattedTime += NAMES_OF_THE_HOURS[hour % NAMES_OF_THE_HOURS.length];
         }  else {
-            output += hours[(hour+1)%12];
+            formattedTime += NAMES_OF_THE_HOURS[(hour+1) % NAMES_OF_THE_HOURS.length];
         }
 
-        output += " " + dayQuadrant[hour/6];
+        formattedTime += " " + QUADRANTS_OF_THE_DAY[hour/HOURS_IN_A_QUARTER_OF_A_DAY];
 
-        return output;
+        return formattedTime;
     }
-
 }
--- src/test/java/test/com/solutionsiq/timeofday/TimeOfDayServiceTest.feature
+++ src/test/java/test/com/solutionsiq/timeofday/TimeOfDayServiceTest.feature
@@ -8,15 +8,15 @@ Feature: Time of day formatted as spoken words
     And I should see spoken formatting of "<result>"
 
     Examples:
-      |hour|minute|second|result                                 |
-      |9   |0     |0     |nine in the morning                    |
-      |20  |4     |45    |almost ten after eight in the evening  |
-      |10  |11    |12    |ten after ten in the morning           |
-      |22  |12    |12    |ten after ten in the evening           |
-      |10  |14    |35    |a quarter after ten in the morning     |
-      |5   |21    |18    |twenty after five at night             |
-      |23  |25    |25    |almost half past eleven in the evening |
-      |15  |31    |31    |half past three in the afternoon       |
-      |9   |34    |33    |almost twenty before ten in the morning|
-      |21  |46    |0     |a quarter of ten in the evening        |
-      |11  |51    |10    |ten of twelve in the morning           |
+      |hour|minute|second|result                                   |
+      |9   |3     |0     |a little after nine in the morning       |
+      |20  |4     |45    |a little after eight in the evening      |
+      |10  |11    |12    |about ten after ten in the morning       |
+      |22  |12    |12    |about ten after ten in the evening       |
+      |10  |14    |35    |about a quarter after ten in the morning |
+      |5   |21    |18    |about twenty after five at night         |
+      |23  |25    |25    |almost half past eleven in the evening   |
+      |15  |31    |31    |about half past three in the afternoon   |
+      |9   |34    |33    |almost twenty before ten in the morning  |
+      |21  |46    |0     |about a quarter of ten in the evening    |
+      |11  |51    |10    |about ten of twelve in the morning       |
